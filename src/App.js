bae7ac3f184e5b3e0b8be6b9863334a2f639c45c
Devika Kumari <devika21@navgurukul.org>
2024-10-15 14:31:19 +0530

    Fetching data from the API and displaying it in a table format

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([])
+
+  const fetchData = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data)
+        setData(data)
+      })
+      .catch(error => {
+        console.error('There was a problem with your fetch operation:', error);
+      })
+  }
+  fetchData()
 
 
   return (
     <div className="app">
-      <table  border="1" cellPadding="10">
+      <table border="2" cellPadding="10">
         <thead>
           <tr>
             <th>ID</th>
@@ -15,11 +30,13 @@ function App() {
         </thead>
 
         <tbody>
-          <tr>
-            <td className='td-text1'></td>
-            <td className='td-text1'></td>
-            <td></td>
-          </tr>
+          {data.map((text) => (
+            <tr key = {text.id}>
+              <td className='td-text1'>{text.id}</td>
+              <td>{text.title}</td>
+              <td>{text.completed.toString()}</td>
+            </tr>
+          ))}
         </tbody>
 
       </table>
