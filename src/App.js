5b46ffda76ba2879caea290a67902f2bc8bab9f9
Devika Kumari <devika21@navgurukul.org>
2024-10-15 12:52:22 +0530

    UI is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 
 function App() {
-  
+
+
   return (
-    <div className="App">
-     
+    <div className="app">
+      <table  border="1" cellPadding="10">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Completed</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td className='td-text1'></td>
+            <td className='td-text1'></td>
+            <td></td>
+          </tr>
+        </tbody>
+
+      </table>
     </div>
   );
 }
 
 export default App;
+
